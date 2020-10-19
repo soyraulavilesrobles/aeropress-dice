7908f1ccaa1569aa85791b1d6f719c7857f476ef
soyraulavilesrobles <73067907+soyraulavilesrobles@users.noreply.github.com>
2020-10-19 07:18:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={aeropress} className="App-logo" alt="logo" />
-          <h1 className="App-title">Random Aeropress Recipe</h1>
         </header>
         <Recipe/>
         <footer className="App-footer">
           <p>
-            Built with &hearts; by <a href="https://github.com/thewinniewu">thewinniewu</a>
             <br/>
              Inspired by <a href="https://web.archive.org/web/20171208104650/https://jimseven.com/2017/12/06/coffee-brewing-dice/" target="_blank" rel="noopener noreferrer">
                James Hoffmann's Coffee Brewing Dice
